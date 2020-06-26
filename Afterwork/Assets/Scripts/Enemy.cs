7a6a91dd6d80ab4701f553cd6f061ebe0ac1119b
Johan Plasencia <plasenciajohan@hotmail.com>
2020-06-26 14:09:38 +0200

    Initial version with demo scene

---
+++ Afterwork/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    private GameObject player;
+    Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+            Debug.Log("damage player");
+    }
+}
---
+++ Afterwork/Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag.Equals("Player"))
+            Debug.Log("PEGFA");
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        
+    }
+}
